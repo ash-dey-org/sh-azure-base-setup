ce9ff00fb3964f17693a37a0d0f75cc91b639f09
Ash Dey <ashabc [at] hotmail.com>
2023-04-16 18:45:31 +1000

    fixed existing role assignment

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# sh-azure-base-setup
-Create SP, RG, assign role to RG using shell script
+### Used for initial setup of Azure Resource Group before Terraform Cloud can be used to deploy resources.
+
+
+script usage:
+<script_name> <Service_Principal_name> <Resource_Group_name>
+
+Creates Azure Resource Group, if it does not exist already.
+Creates Service Principal (rbac), if it does not exist already.
+Assigns contributor role to service principal to the resource group.
+
+
--- az-sp-rg-setup.sh
+++ az-sp-rg-setup.sh
@@ -7,8 +7,8 @@ if [ $# -ne 2 ]; then
     echo "This script requires two arguments"
     echo "The first argument - display name of the Azure service principal (tf-np-sp-xxx-VS-AD)"
     echo "The second argument - name of the Azure resource group (d-auea-sa-rg-xxxx)"
-    echo "if the resources already exist, it will assign SP contributor role to RG"
-    echo "if the resources does not exist, then it will create resources and assign permission"
+    echo "if the resource group already exist, it will assign SP contributor role to RG"
+    echo "if the resource group does not exist, then it will create resources and assign permission"
     exit 0
 fi
 
@@ -17,28 +17,38 @@ if [ $(az group exists --name $2) = true ]
 
     then
         echo Resource Group $2 already exists
-        echo "Extracting subscription id"
+        # echo "Extracting subscription id"
         subs_id=$(jq -r .id <<< "$(az account show)")
 
     else
         echo "Resource Group $2 does not exist"
         read -p "Enter the Azure region (AustraliaEast|AustraliaSouthEast) to create resource group: " location
         echo creating Resource Group $2 in region $location
-        # rg_output=$(jq -r .id <<< "$(az group create --name $2 --location $location)")
-        # subs_id=$(echo $rg_output | cut -d/ -f3)
+        rg_output=$(jq -r .id <<< "$(az group create --name $2 --location $location)")
+        subs_id=$(echo $rg_output | cut -d/ -f3)
 fi
-
 
 if [[ $(az ad sp list --display-name $1) != '[]' ]]
 # if az ad sp list --display-name $1 | grep -Pq '"displayName":'
     then
-        echo "Service Principal $1 already exists, assigning permission"
-        # az role assignment create --assignee $1 --role Contributor --scope /subscriptions/$subs_id/resourceGroups/$2
+        echo "Service Principal $1 already exists, checking if role assignment exists"
+        appId=$(jq -r .[].appId <<< "$(az ad sp list --display-name $1)")
+        RGappID=$(jq -r ".[] | select(.principalName==\"$appId\") | .principalName" <<< "$(az role assignment list --resource-group $2)")
+        # echo RG appId $RGappID
+        if [[ $RGappID = $appId ]]
+            then
+                echo "role assignement already exists"
+
+            else
+                echo "role assignement does not exist, creating role assignment"
+                az role assignment create --assignee $appId --role Contributor --scope /subscriptions/$subs_id/resourceGroups/$2
+                # az role assignment list --resource-group $2
+        fi
 
     else
         echo "Service Principal $1 does not exist"
         echo "creating service principal $1 and assigning permission"
-        # az ad sp create-for-rbac -n $1 --role Contributor --scopes /subscriptions/$subs_id/resourceGroups/$2
+        az ad sp create-for-rbac -n $1 --role Contributor --scopes /subscriptions/$subs_id/resourceGroups/$2
 fi
 
 
